b3d46febd4873ee2c9da8f9de0d641f540b9a5c8
86139@cz <1620645082@qq.com>
2024-01-21 10:26:51 +0800

    temp

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -1,66 +1,110 @@
 const app = getApp();
+let name, description, price;
+
 Page({
+  getName(event) {
+    console.log(event.detail.value);
+    name: event.detail.value;
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    getDescription(event) {
+      description: event.detail.value;
+    },
 
+    getPrice(event) {
+      price: event.detail.value;
+    },
+
+
+  // 选择图片
+  chooseImage: function () {
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        that.setData({
+          tempFilePath: res.tempFilePaths[0]
+        });
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
 
-  },
+  submit: function () {
+    var that = this;
+    // 上传图片
+    wx.uploadFile({
+      url: 'http://localhost:8080/updateGoodsImage',
+      filePath: that.data.tempFilePath,
+      name: 'file',
+      formData: {
+        goodsName: encodeURIComponent(name) // 编码商品名
+      },
+      success: function (res) {
+        // 直接使用后端返回的文件路径
+        var data = res.data;
+        console.log(data);
+        if (data) {
+          that.setData({
+            imageUrl: 'http://localhost:8080/' + data
+          });
+          wx.showToast({
+            title: '上传成功',
+            icon: 'success',
+            duration: 2000
+          });
+        } else {
+          wx.showToast({
+            title: '上传失败',
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: '上传失败',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+    
+    console.log(username, age, id, password,)
+    wx.request({
+      url: 'http://localhost:8080/add',
+      method: 'GET',
+      data: {
+        name: username,
+        age,
+        id,
+        password,
+      },
+      success(res){
+        console.log("添加成功", res)
+      },
+      fail(res){
+        console.log("添加失败", res)
+      }
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    wx.request({
+      url: 'http://localhost:8080/addGoods',
+      method: 'GET',
+      data: {
+        name
+      },
+      success(res){
+        console.log("添加成功", res)
+      },
+      fail(res){
+        console.log("添加失败", res)
+      }
+    })
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
 
   }
 })
